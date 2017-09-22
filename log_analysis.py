7c9301b42e27f3dbb2ed419dad68f2f7c3a4b97a
Ong Kuo Han <kuohan95@gmail.com>
2017-09-22 13:35:35 +0100

    Updated Python code to more modular.
    Divided code into separate functions in order  to make it more modular.

--- log_analysis.py
+++ log_analysis.py
@@ -1,64 +1,108 @@
 #!/usr/bin/env python2
-from datetime import datetime
 import psycopg2
 
-# Name of the database
 DBNAME = "news"
 
-# Connect to databse
-db = psycopg2.connect(database=DBNAME)
-c = db.cursor()
 
-# Query 1
-# To find three most popular articles of all time
-c.execute("""
-SELECT title, views
-FROM articles
-INNER JOIN
-    (SELECT path, count(path) AS views
-    FROM log
-    GROUP BY log.path) AS log
-    ON log.path = '/article/' || articles.slug
-    ORDER BY views DESC
-    LIMIT 3;
-""")
-result = c.fetchall()
-print("What are the most popular three articles of all time?")
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+        as well as a cursor for the database.
 
-for title, views in result:
-    print('\n\t{} - {} views'.format(title, views))
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
+    """
 
-# Query 2
-# To find the most popular article authors of all time
-c.execute("""
-SELECT authors.name, COUNT(*) AS num
-FROM authors, articles, log
-WHERE log.path = '/article/' || articles.slug
-AND authors.id = articles.author
-GROUP BY authors.name
-ORDER BY num DESC;
-""")
-result = c.fetchall()
-print("\nWho are the most popular article authors of all time?")
+    # Your code here
+    # Connect to databse
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    return c, db
 
-for title, views in result:
-    print('\n\t{} - {} views'.format(title, views))
 
-# Query 3
-# To calculate which days did more than 1% of requests lead to error
-# By making use of VIEW refer to readme.md
-c.execute("""
-SELECT date, percentage
-FROM rate
-WHERE rate.percentage > 1
-GROUP BY date, percentage
-ORDER by date;
-""")
-result = c.fetchall()
-print("\nOn which days did more than 1% of requests lead to errors")
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+       args:
+           query - an SQL query statement to be executed.
 
-for date, error_percent in result:
-    print('\n\t{0:%B %d, %Y} - {1:.1f}% errors'.format(date, error_percent))
+       returns:
+           A list of tuples containing the results of the query.
+    """
 
-db.commit()
-db.close()
+    # Your code here
+    c, db = db_connect()
+    c.execute(query)
+    result = c.fetchall()
+    return result
+    db.commit()
+    db.close()
+
+
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+
+    query = """
+    SELECT title, views
+    FROM articles
+    INNER JOIN
+        (SELECT path, count(path) AS views
+        FROM log
+        GROUP BY log.path) AS log
+        ON log.path = '/article/' || articles.slug
+        ORDER BY views DESC
+        LIMIT 3;
+    """
+
+    results = execute_query(query)
+
+    # add code to print results
+    print("What are the most popular three articles of all time?")
+
+    for title, views in results:
+        print('\n\t{} - {} views'.format(title, views))
+
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = """
+    SELECT authors.name, COUNT(*) AS num
+    FROM authors, articles, log
+    WHERE log.path = '/article/' || articles.slug
+    AND authors.id = articles.author
+    GROUP BY authors.name
+    ORDER BY num DESC;
+    """
+
+    results = execute_query(query)
+
+    # add code to print results
+    print("\nWho are the most popular article authors of all time?")
+
+    for title, views in results:
+        print('\n\t{} - {} views'.format(title, views))
+
+
+def print_errors_over_one():
+    """Prints out the days where more than 1% of logged
+    access requests were errors."""
+    query = """
+    SELECT date, percentage
+    FROM rate
+    WHERE rate.percentage > 1
+    GROUP BY date, percentage
+    ORDER by date;
+    """
+    results = execute_query(query)
+
+    # add code to print results
+    print("\nOn which days did more than 1% of requests lead to errors")
+
+    for date, error_percent in results:
+        print(
+            '\n\t{0:%B %d, %Y} - {1:.1f}% errors'.format(date, error_percent))
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
